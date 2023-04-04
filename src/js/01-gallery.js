0706c51c81cdffb70ecee188c7f65e4cd6df0d98
Gabriel Tur <tur.gabriel90@gmail.com>
2023-04-04 22:05:35 +0200

    Add task 1, 2 and 3

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,21 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// Add imports above this line
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+galleryItems.forEach(item => {
+  const galleryItem = `<div ><a class="gallery__item" href="${item.original}"><img class="gallery__image" src="${item.preview}" 
+  alt="${item.description}"/></a></div>`;
+  gallery.insertAdjacentHTML('beforeend', galleryItem);
+});
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionType: 'alt',
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,47 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailArea = document.querySelector('input');
+const messageArea = document.querySelector('textarea');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(dataSave, 500));
+
+function dataSave() {
+  const mailValue = emailArea.value;
+  const messageValue = messageArea.value;
+  const formData = {
+    email: mailValue,
+    message: messageValue,
+  };
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+}
+
+function readData() {
+  const saveFormData = localStorage.getItem(LOCALSTORAGE_KEY);
+  if (saveFormData) {
+    const formData = JSON.parse(saveFormData);
+    mailValue.value = formData.email;
+    messageValue.value = formData.message;
+  }
+}
+readData();
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const formElements = event.currentTarget.elements;
+  const email = formElements.email.value;
+  const message = formElements.message.value;
+  if (email.length === 0 || message.length === 0) {
+    alert('Please fill in all fields');
+  } else {
+    const formData = {
+      email,
+      message,
+    };
+    console.log(formData);
+    form.reset();
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+  }
+}
