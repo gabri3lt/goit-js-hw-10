63120e1d74565f283eca04bf1e4e4bdb22c041a9
Gabriel Tur <tur.gabriel90@gmail.com>
2023-04-07 22:21:12 +0200

    Timer and promisse check

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
+import Notiflix from 'notiflix';
+
+const refs = {
+  form: document.querySelector('.form'),
+  firstDelayInput: document.querySelector('input[name="delay"]'),
+  delayStepInput: document.querySelector('input[name="step"]'),
+  promiseAmountInput: document.querySelector('input[name="amount"]'),
+};
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+refs.form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const position = Number(refs.promiseAmountInput.value);
+  let delay = Number(refs.firstDelayInput.value);
+  const step = Number(refs.delayStepInput.value);
+
+  for (let i = 1; i <= position; i += 1) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+      });
+
+    delay += step;
+  }
+});
